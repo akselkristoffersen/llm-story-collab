797a1ba08c94616fa775cefdc77c3fb174e9076c
Aksel Kristoffersen <56234786+aksekris@users.noreply.github.com>
2024-10-08 17:46:09 +0200

    Add backend-server-py

---
+++ backend-server-py/server.py
@@ -0,0 +1,14 @@
+import asyncio
+import websockets
+
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send(message)
+
+async def main():
+    async with websockets.serve(echo, "", 8080):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
