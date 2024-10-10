d376eb69bf299aeee6520f5c7b90cf2b854a67d7
Aksel Kristoffersen <56234786+aksekris@users.noreply.github.com>
2024-10-10 21:53:50 +0200

    Update backend-server-py to use model

--- backend-server-py/server.py
+++ backend-server-py/server.py
@@ -1,9 +1,41 @@
+import os
 import asyncio
 import websockets
+import torch
+from transformers import pipeline
+
+model_directory = os.environ["AX_MODEL_PATH"]
+
+generator = pipeline(
+    "text-generation",
+    model=model_directory,
+    tokenizer=model_directory,
+    model_kwargs={"torch_dtype": torch.float16},
+    device_map="auto"
+)
+
+generator_lock = asyncio.Lock()
+
+async def run_inference(monologue):
+    loop = asyncio.get_event_loop()
+    async with generator_lock:
+        monologue = await loop.run_in_executor(None, lambda : generator(
+                    monologue,
+                    max_new_tokens=5,
+                    num_return_sequences=1,
+                    do_sample=False,
+                    truncation=True,
+                    pad_token_id = generator.tokenizer.eos_token_id
+                )[0]['generated_text'])
+    return monologue
 
 async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
+    monologue = ""
+    async for client_contribution in websocket:
+        monologue += client_contribution
+        monologue_size = len(monologue)
+        monologue = await run_inference(monologue)
+        await websocket.send(monologue[monologue_size:])
 
 async def main():
     async with websockets.serve(echo, "", 8080):
--- docker/backend-server-py.Dockerfile
+++ docker/backend-server-py.Dockerfile
@@ -1,11 +1,8 @@
 FROM python:3.10-slim
 WORKDIR /app
 COPY backend-server-py/requirements.txt .
-RUN python3 -m venv /venv && \
-    . /venv/bin/activate && \
-    pip install --upgrade pip && \
+RUN pip install --upgrade pip && \
     pip install -r requirements.txt
-
 COPY backend-server-py .
 EXPOSE 8080
-CMD ["/bin/bash", "-c", "source /venv/bin/activate && python3 server.py"]
+CMD ["python3", "server.py"]
